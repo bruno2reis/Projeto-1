debb5613dddb9e95838e800238a2809cfd98d4c0
Bruno dos Reis <brunodosreis21@gmail.com>
2016-11-29 15:46:15 -0200

    F1 novo.

---
+++ pedidos.js
@@ -0,0 +1,16 @@
+function enviar_pedido(){
+    var nome = document.getElementById("name_id");
+
+    if (nome.value != ""){
+        alert("Obrigado Sr(a) " + nome.value + "os seus dados foram salvos com sucesse!");
+
+    }
+    var fone = document.getElementById("tel_id");
+
+    if (fone.value != "",git ){
+        alert("erro");
+    }
+
+
+}
+
